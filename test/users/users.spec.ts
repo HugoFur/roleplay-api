d097bbdc7290d5a090e56d75385c299449c02e7b
Hugo Furtado <hugofurtadocos@gmail.com>
2024-01-06 14:42:27 -0300

    test: add user avatar to user creation test

--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -3,12 +3,19 @@ import test from 'japa'
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 test.group('Users', () => {
-  test('it should create an user', async () => {
+  test('it should create an user', async (assert) => {
     const userPayload = {
       email: 'test@test.com',
       username: 'test',
       password: 'test',
+      avatar: 'https://www.gravatar.com/ava',
     }
-    await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+    const { body } = await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+    assert.exists(body.user, 'User undefined')
+    assert.exists(body.user.id, 'User id undefined')
+    assert.equal(body.user.email, userPayload.email)
+    assert.equal(body.user.username, userPayload.username)
+    assert.equal(body.user.password, userPayload.password)
+    assert.equal(body.user.avatar, userPayload.avatar)
   })
 })
